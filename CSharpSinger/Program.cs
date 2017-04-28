ea05bcfcbaf0f66fbe89ba9a0c87bf28400c17e3
Eric J Fisher <EricJFisher@Live.com>
2017-04-28 13:21:53 -0400

    Initial Commit
    The basic application is complete, however; songs have no timing applied
    to them yet.

---
+++ CSharpSinger/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpSinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = { "allthistime", "codemonkey", "futuresoon" };
+            Console.WriteLine("Welcome to Singer!");
+            
+            while (true)
+            {
+                Console.WriteLine("Please choose the song that you'd like sung!");
+                Console.WriteLine("1 for Jonathan Coulton's 'All This Time'");
+                Console.WriteLine("2 for Jonathan Coulton's 'Code Monkey'");
+                Console.WriteLine("3 for Jonathan Coulton's 'Future Soon'");
+                Console.WriteLine("Enter the track number of the song you'd like or 'Quit'");
+                var response = Console.ReadLine();
+
+                if (response.ToLower() == "quit") // new method
+                    break;
+
+                var song = 0;
+                if(!int.TryParse(response, out song))
+                {
+                    Console.WriteLine("Invalid response, try again.");
+                    continue;
+                }
+                else
+                    song--; //Arrays count from zero, people don't
+
+                // set the directory to something for yourself
+                var fileName = @"Songs\" + songs[song] + ".txt";
+
+                if (!File.Exists(fileName)) // new condition type
+                {
+                    Console.WriteLine("File not found, try again");
+                    continue; // new keyword
+                }
+
+                Console.WriteLine("===============================");
+                var lines = File.ReadAllLines(fileName); // new method
+                foreach (var line in lines)
+                {
+                    Singer.WriteLine(line);
+                }
+                Console.WriteLine("===============================");
+            }
+        }
+    }
+}
---
+++ CSharpSinger/Singer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpSinger
+{
+    public static class Singer
+    {
+        public static void WriteLine(string line)
+        {
+            var results = line.Split(':');
+            WriteLine(results[0], int.Parse(results[1]));
+        }
+
+        public static void WriteLine(string line, int speed)
+        {
+            foreach(var letter in line.ToCharArray())
+            {
+                Console.Write(letter);
+                if (speed > 50)
+                    speed = 50;
+                else if (speed < 1)
+                    speed = 1;
+                System.Threading.Thread.Sleep((50-speed) * 10); // new method
+            }
+            Console.WriteLine("");
+        }
+    }
+}
